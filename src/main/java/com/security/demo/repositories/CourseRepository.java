4d8def2339bff2e487d244aa4153b89f05c482b4
mohammed.yasser <mohammedre4a@gmail.com>
2025-05-24 18:59:51 +0300

    modify repositories to extend CRUDRepository

--- src/main/java/com/security/demo/repositories/CourseRepository.java
+++ src/main/java/com/security/demo/repositories/CourseRepository.java
@@ -2,9 +2,12 @@ package com.security.demo.repositories;
 
 import com.security.demo.entity.CourseEntity;
 import com.security.demo.model.Course;
-import com.security.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
+import java.util.Optional;
 
+@Repository
+public interface CourseRepository extends CrudRepository<CourseEntity, Long> {
+    Optional<Course> findCourseByName(String name);
 }
--- src/main/java/com/security/demo/repositories/StudentRepository.java
+++ src/main/java/com/security/demo/repositories/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.security.demo.repositories;
 
 import com.security.demo.entity.StudentEntity;
-import com.security.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+@Repository
+public interface StudentRepository extends CrudRepository<StudentEntity, Long> {
 
 }
